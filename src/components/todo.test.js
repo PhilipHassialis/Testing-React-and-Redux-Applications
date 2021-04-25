544d693db55616c51a7ba664520ee2292caffef2
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-04-25 20:57:35 +0300

    mini lint issue

--- src/components/todo.test.js
+++ src/components/todo.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { configure, mount, shallow } from "enzyme";
+import { configure, shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import Todo from "./Todo";
 
