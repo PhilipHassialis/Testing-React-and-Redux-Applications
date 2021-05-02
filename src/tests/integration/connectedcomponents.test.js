81ad25b94fb0179fe47e92ec2c376021e73bde31
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-05-02 20:32:29 +0300

    Full DOM integration tests

---
+++ src/tests/integration/connectedcomponents.test.js
@@ -0,0 +1,26 @@
+import "jsdom-global/register";
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "../../components/App";
+import rootReducer from "../../reducers";
+import { screen, render, fireEvent } from "@testing-library/react";
+
+const store = createStore(rootReducer);
+
+describe("connected component full app integration tests", () => {
+    it("should add Todo using fireEvent", () => {
+        const { getByTestId, getByText, container } = render(
+            <Provider store={store}>
+                <App />
+            </Provider>
+        );
+        const inputElement = getByTestId("add-todo-input");
+        fireEvent.change(inputElement, { target: { value: "go walking" } });
+        fireEvent.click(getByText("Add Todo"));
+        expect(store.getState().todos.length).toEqual(1);
+
+        const liElement = container.querySelector("li");
+        expect(liElement.textContent).toBe("go walking");
+    });
+});
